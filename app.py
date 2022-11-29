4405d40682b8d65553628b4f13cab1a31718f0c7
monees007 <monees007@outlook.com>
2022-11-29 20:32:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 st.header('ODD or EVEN')
 
 number = st.number_input('Enter number to proceed')
-if number%2:
-  res='EVEN'
-else:
-  res='ODD'
-  
-st.write(res)
 
+def trb:
+  if number%2:
+    res='EVEN'
+  else:
+    res='ODD'
+
+  st.write(res)
+
+st.button("Check", onclick=trb)
